6a658d4e42010913fd0b08b79199718f00e0ee5e
leo <leo@leodeMacBook-Pro.local>
2021-10-20 11:53:25 +0800

    update

--- cache_util/goredis/cluster/ttl.go
+++ cache_util/goredis/cluster/ttl.go
@@ -4,12 +4,13 @@ import (
 	"context"
 	"math/rand"
 
+	gofastcache "github.com/daqnext/go-fast-cache"
+	"github.com/daqnext/utils/cache_util"
 	"github.com/go-redis/redis/v8"
 )
 
 //for redis cluster , when return true , you need to refresh the key-value
 func CheckTtlRefresh(Redis *redis.ClusterClient, ctx context.Context, keystr string) bool {
-
 	if rand.Intn(10) == 1 {
 		Duration, error := Redis.TTL(ctx, keystr).Result()
 		if error != nil {
@@ -28,4 +29,15 @@ func CheckTtlRefresh(Redis *redis.ClusterClient, ctx context.Context, keystr str
 	} else {
 		return false
 	}
+}
+
+func SmartCheck_LocalCache_Redis(ctx context.Context, Redis *redis.ClusterClient, LocalCache *gofastcache.LocalCache, keystr string) (interface{}, int64, bool) {
+	localvalue, ttl, localexist := LocalCache.Get(keystr)
+	if !cache_util.CheckTtlRefresh(ttl) && localexist {
+		rresult, err := Redis.Get(ctx, keystr).Result()
+		if err == nil && rresult == LocalCache.GetRand(keystr) {
+			return localvalue, ttl, true
+		}
+	}
+	return nil, 0, false
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/daqnext/utils
 
 go 1.16
 
-require github.com/go-redis/redis/v8 v8.11.4
+require (
+	github.com/daqnext/go-fast-cache v1.0.6
+	github.com/go-redis/redis/v8 v8.11.4
+)
